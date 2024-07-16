454068799ac780aa14392a6b34a84062332e9a26
Isaac-Amano <yishakamano2020@gmail.com>
2024-07-16 02:01:47 -0500

    console logged collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -20,9 +20,58 @@ console.log(addToCollection(myCollection, "2016 Forrest Hills Drive", "J cole", 
 console.log(addToCollection(myCollection, "Ethiopia", "Teddy Afro", 2018));
 console.log(addToCollection(myCollection, "WWTTBA", "Drake", 1976));
 
+console.log(myCollection);
 
 
+function findByArtist(collection, artist) {
+  let results = [];
+  for (let album of collection) {
+      if (album.artist === artist) {
+       // if (album.artist = artist) {
+          results.push(album);
+      }
+  }
+  return results;
+}
 
+console.log(findByArtist(myCollection, "Drake")); 
+console.log(findByArtist(myCollection, "J cole")); 
+
+function search(collection, searchCriteria) {
+   
+  if (!searchCriteria || (!searchCriteria.artist && !searchCriteria.yearPublished)) {
+    //if (searchCriteria || (searchCriteria.artist && searchCriteria.yearPublished)) {
+        return collection;
+    }
+
+    let results = [];
+    for (let album of collection) {
+        let artistMatches = true;
+        let yearMatches = true;
+ // for of loop
+        if (searchCriteria.artist) {
+            artistMatches = album.artist === searchCriteria.artist;
+        }
+
+        if (searchCriteria.yearPublished) {
+            yearMatches = album.yearPublished === searchCriteria.yearPublished;
+        }
+
+        if (artistMatches && yearMatches) {
+            results.push(album);
+        }
+    }
+
+    return results;
+}
+
+
+console.log(search(myCollection, { artist: "Michael Jackson", yearPublished: 1982 })); 
+console.log(search(myCollection, { artist: "AC/DC", yearPublished: 1980 })); //
+console.log(search(myCollection, { artist: "The Beatles" })); 
+console.log(search(myCollection, { yearPublished: 1977 })); 
+console.log(search(myCollection, {})); 
+console.log(search(myCollection)); 
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
